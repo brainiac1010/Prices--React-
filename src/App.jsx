4ef3a9122fdf06fb03d914c494550f622e831604
Arman Hossain <armanmahim00@gmail.com>
2024-11-25 22:17:37 +0600

    start commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <h1 className='text text-5xl bg-amber-300'>Price Option</h1>
+     
+
+    </>
+  )
+}
+
+export default App
