96f017858db633fe3b3b54dc8e2005625dbc98b3
Arman Hossain <armanmahim00@gmail.com>
2024-11-25 23:11:44 +0600

    navbar added with route

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 
 
 import './App.css'
+import Nav from './Components/nav/nav'
+import NavBar from './Components/NavBar/NavBar'
 
 function App() {
   
 
   return (
     <>
-      
+      {/* <Nav></Nav> */}
+      <NavBar></NavBar>
       <h1 className='text text-5xl bg-amber-300'>Price Option</h1>
      
 
