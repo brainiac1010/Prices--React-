af0d9837f171ce8ddf42c6972093fe8f95242684
Arman Hossain <armanmahim00@gmail.com>
2024-11-30 20:19:34 +0600

    spiner added

--- src/Components/Phones/Phones.jsx
+++ src/Components/Phones/Phones.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Radar, RadarChart, PolarGrid, Legend, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer, Tooltip } from 'recharts';
-
+import { DNA } from 'react-loader-spinner'
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone') 
@@ -16,18 +18,35 @@ const Phones = () => {
                 const phonesWithFakeData = phoneData.map(phone => {
                     const obj = {
                         name: phone.phone_name,
-                        price: parseInt(phone.slug.split('-')[1]) || Math.floor(Math.random() * 1000) + 100 
+                        price: parseInt(phone.slug.split('-')[1]) || Math.floor(Math.random() * 1000) + 100
                     };
                     return obj;
                 });
                 console.log(phonesWithFakeData);
                 setPhones(phonesWithFakeData);
+                setLoading(false);
             })
             .catch(error => console.error("Error fetching phone data:", error));
     }, []);
 
     return (
-        <div>
+        <div >
+
+
+            {loading && <div className="flex items-center justify-center h-screen ">
+
+                <DNA
+                    visible={true}
+                    height="80"
+                    width="80"
+                    ariaLabel="dna-loading"
+                    wrapperStyle={{}}
+                    wrapperClass="dna-wrapper"
+                />
+            </div>
+
+            }
+
             <h2 className="text-4xl text-center mb-8">Phones: {phones.length}</h2>
             <ResponsiveContainer width="100%" height={400}>
                 <RadarChart cx="50%" cy="50%" outerRadius="80%" data={phones}>
